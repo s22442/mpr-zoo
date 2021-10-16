381ffd5c0ca7c4ee213d2eea6d5926a15e13b857
s22442 <s22442@pjwstk.edu.pl>
2021-10-16 17:49:33 +0200

    change demo to zoo

--- src/main/java/com/example/zoo/ZooApplication.java
+++ src/main/java/com/example/zoo/ZooApplication.java
@@ -1,11 +1,12 @@
-package com.example.demo;
+package com.example.zoo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ZooApplication {
   public static void main(String[] args) {
-    SpringApplication.run(DemoApplication.class, args);
+    SpringApplication.run(ZooApplication.class, args);
+    System.out.println("Started Zoo Application");
   }
 }
